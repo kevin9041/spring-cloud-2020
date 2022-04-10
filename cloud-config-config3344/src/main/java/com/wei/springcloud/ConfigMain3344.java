718049b98f9510eefeea71eb5099227a5959d2e3
魏焕波 <635388281@qq.com>
2022-04-10 21:09:03 +0800

    config配置中心基础配置

---
+++ cloud-config-config3344/src/main/java/com/wei/springcloud/ConfigMain3344.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2018-2022 All Rights Reserved.
+ */
+package com.wei.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author weihuanbo
+ * @version ConfigMain3344: ConfigMain3344.java, v0.1 2022/4/10 weihuanbo Exp $$
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigMain3344.class, args);
+    }
+
+}
