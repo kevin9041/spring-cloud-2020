9016856653717107a905ab78e8c00333233ae46f
魏焕波 <635388281@qq.com>
2022-04-20 01:02:26 +0800

    config配置中心基础配置-手动更新配置

--- cloud-config-config3355/src/main/java/com/wei/springcloud/controller/ConfigClientController.java
+++ cloud-config-config3355/src/main/java/com/wei/springcloud/controller/ConfigClientController.java
@@ -4,6 +4,7 @@
 package com.wei.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @version ConfigClientController: ConfigClientController.java, v0.1 2022/4/19 weihuanbo Exp $$
  */
 @RestController
+@RefreshScope
 public class ConfigClientController {
     @Value("${foo}")
     private String configInfo;
