cfdbb1be5f108c9a4bebafccd886ba177cb43316
魏焕波 <635388281@qq.com>
2022-05-02 00:45:12 +0800

    一些调整

--- cloud-eureka-consumer-order80/src/main/java/com/wei/springcloud/config/ApplicationContextConfig.java
+++ cloud-eureka-consumer-order80/src/main/java/com/wei/springcloud/config/ApplicationContextConfig.java
@@ -15,7 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ApplicationContextConfig {
     @Bean
-    //@LoadBalanced //虽然服务注册进了eureka，服务的调用方可以通过注册时的application-name定位到服务的提供者，
+    @LoadBalanced //虽然服务注册进了eureka，服务的调用方可以通过注册时的application-name定位到服务的提供者，
                     // 但是具体调用哪个端口不清楚，所以需要开启负载均衡策略，默认使用轮询方式
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
