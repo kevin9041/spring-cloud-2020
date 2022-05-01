2041892f8eafd42a6360ed07f2986b03ee1e3ada
魏焕波 <635388281@qq.com>
2022-05-01 22:02:07 +0800

    stream消费者服务构建

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/wei/springcloud/service/MessageReceiveService.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2018-2022 All Rights Reserved.
+ */
+package com.wei.springcloud.service;
+
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author weihuanbo
+ * @version MessageReceiveService: MessageReceiveService.java, v0.1 2022/5/1 weihuanbo Exp $$
+ */
+@EnableBinding(Sink.class) //定义消息的接收管道
+@Component
+public class MessageReceiveService {
+
+    @StreamListener(Sink.INPUT)
+    public void messageListener(Message<String> message) {
+        System.out.println("*****message:" + message.getPayload());
+    }
+}
