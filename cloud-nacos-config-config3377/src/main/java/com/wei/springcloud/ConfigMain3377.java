8d78c47976c4ce7a4a773ae870028783220878c0
魏焕波 <635388281@qq.com>
2022-05-03 21:42:02 +0800

    nacos的config配置-dataid

---
+++ cloud-nacos-config-config3377/src/main/java/com/wei/springcloud/ConfigMain3377.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) 2018-2022 All Rights Reserved.
+ */
+package com.wei.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author weihuanbo
+ * @version ConfigMain3355: ConfigMain3355.java, v0.1 2022/4/10 weihuanbo Exp $$
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConfigMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigMain3377.class, args);
+    }
+
+}
